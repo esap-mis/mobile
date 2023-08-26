5c4c7909edcd0c91532c4e149492043386111aa8
thewyolar <thewyolar@gmail.com>
2023-08-26 18:18:51 +0300

    feat: add view model to perform login

---
+++ app/src/main/java/javavlsu/kb/esap/esapmobile/data/AuthViewModel.kt
@@ -0,0 +1,28 @@
+package javavlsu.kb.esap.esapmobile.data
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import javavlsu.kb.esap.esapmobile.network.Api
+import javavlsu.kb.esap.esapmobile.network.model.AuthRequest
+import kotlinx.coroutines.launch
+
+class AuthViewModel : ViewModel() {
+    fun performLogin(username: String, password: String, onResult: (String) -> Unit) {
+        val loginRequest = AuthRequest(username, password)
+
+        viewModelScope.launch {
+            try {
+                val response = Api.apiService.login(loginRequest)
+                if (response.isSuccessful) {
+                    val token = response.body()?.jwt ?: ""
+                    onResult("Success: $token")
+                } else {
+                    val error = response.errorBody()?.string()
+                    onResult(error ?: "Login error")
+                }
+            } catch (e: Exception) {
+                onResult("Network error: ${e.message}")
+            }
+        }
+    }
+}
