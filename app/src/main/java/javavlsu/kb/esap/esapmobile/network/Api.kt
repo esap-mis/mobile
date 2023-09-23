dd0fdf850d5d04e0735f9536830a1dd3e4166d76
thewyolar <thewyolar@gmail.com>
2023-09-23 22:38:12 +0300

    feat: add object to hold constants

--- app/src/main/java/javavlsu/kb/esap/esapmobile/network/Api.kt
+++ app/src/main/java/javavlsu/kb/esap/esapmobile/network/Api.kt
@@ -1,10 +1,10 @@
 package javavlsu.kb.esap.esapmobile.network
 
+import javavlsu.kb.esap.esapmobile.util.BASE_URL
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object Api {
-    private val BASE_URL = "http://10.0.2.2:8080/api/"
 
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
