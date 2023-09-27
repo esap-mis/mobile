67de23a96333f46b43e19a12f70d20299229c5e8
thewyolar <thewyolar@gmail.com>
2023-09-27 20:47:11 +0300

    chore: update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,17 +3,16 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("dagger.hilt.android.plugin")
     kotlin("kapt")
-//    id("com.google.devtools.ksp") version "1.8.10-1.0.9"
 }
 
 android {
     namespace = "javavlsu.kb.esap.esapmobile"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "javavlsu.kb.esap.esapmobile"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -50,8 +49,8 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
@@ -66,19 +65,19 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
 
     // Hilt
-    val hiltVersion = "2.44"
+    val hiltVersion = "2.46"
     implementation("com.google.dagger:hilt-android:$hiltVersion")
     kapt("com.google.dagger:hilt-android-compiler:$hiltVersion")
 
     // Navigation Compose
-    implementation("androidx.navigation:navigation-compose:2.6.0")
+    implementation("androidx.navigation:navigation-compose:2.7.3")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0-alpha01")
 
     //DataStore
     implementation("androidx.datastore:datastore-preferences:1.0.0")
 
     //OkHttp
-    val okhttpVersion = "4.10.0"
+    val okhttpVersion = "4.11.0"
     implementation("com.squareup.okhttp3:okhttp:$okhttpVersion")
     implementation("com.squareup.okhttp3:logging-interceptor:$okhttpVersion")
 
