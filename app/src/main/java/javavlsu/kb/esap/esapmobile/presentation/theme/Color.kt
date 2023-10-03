297d6f6de33a5ea4d8818bc4ffa1ce15d332de36
thewyolar <thewyolar@gmail.com>
2023-10-03 22:06:38 +0300

    feat: add theme

--- app/src/main/java/javavlsu/kb/esap/esapmobile/presentation/theme/Color.kt
+++ app/src/main/java/javavlsu/kb/esap/esapmobile/presentation/theme/Color.kt
@@ -2,10 +2,13 @@ package javavlsu.kb.esap.esapmobile.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Blue = Color(0xFF377AE4)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Blue80 = Color(0xFF5F93E5)
+val BlueGrey80 = Color(0xFF738FAE)
+
+val Blue40 = Color(0xFF7BA0FF)
+val BlueGrey40 = Color(0xFF8897B2)
+
+val Green80 = Color(0xFF51C456)
+val Gray40 = Color(0xFFDDDDDD)
