55027ffddc363252c81b06ae483c84582a1a5492
thewyolar <thewyolar@gmail.com>
2023-10-22 23:07:39 +0300

    refactor: update size of calendar component

--- app/src/main/java/javavlsu/kb/esap/esapmobile/presentation/component/Calendar.kt
+++ app/src/main/java/javavlsu/kb/esap/esapmobile/presentation/component/Calendar.kt
@@ -42,7 +42,7 @@ fun Calendar(
     val data by calendarViewModel.calendarData.observeAsState()
 
     data?.let { calendarData ->
-        Column(modifier = modifier.fillMaxWidth()) {
+        Column(modifier = modifier.fillMaxWidth(0.9f)) {
             Header(data = calendarData)
             Content(
                 data = calendarData,
