1f84eec91074fd1e4c5cabe151a1cf1b826cdb8c
thewyolar <thewyolar@gmail.com>
2024-05-31 19:10:38 +0300

    chore: update android-ci

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ android {
 
     signingConfigs {
         create("release") {
-            storeFile = file("keystore/ru.esapmobile.jks")
+            storeFile = file("keystore/esapmobile.jks")
             storePassword = System.getenv("SIGNING_STORE_PASSWORD")
             keyAlias = System.getenv("SIGNING_KEY_ALIAS")
             keyPassword = System.getenv("SIGNING_KEY_PASSWORD")
