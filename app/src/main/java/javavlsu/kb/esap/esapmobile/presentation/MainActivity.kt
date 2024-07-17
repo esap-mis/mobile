daa286151f45a99311567c8bebf4e34c0e3cc576
thewyolar <thewyolar@gmail.com>
2024-07-17 20:03:04 +0300

    fix: disable dark theme

--- app/src/main/java/javavlsu/kb/esap/esapmobile/presentation/MainActivity.kt
+++ app/src/main/java/javavlsu/kb/esap/esapmobile/presentation/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            EsapMobileTheme {
+            EsapMobileTheme(darkTheme = false) {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
