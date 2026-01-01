cd8c5c76e99cbeda69a8781e86b3a223b5c9a48e
thewyolar <thewyolar@gmail.com>
2026-01-02 00:17:39 +0300

    migrate to kotlin 2.2.20 and gradle 8.13

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    val kotlinVersion = "2.0.0"
-    id("com.android.application") version "8.4.1" apply false
+    val kotlinVersion = "2.2.20"
+    id("com.android.application") version "8.12.3" apply false
     id("org.jetbrains.kotlin.plugin.compose") version kotlinVersion apply false
     id("org.jetbrains.kotlin.android") version kotlinVersion apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
-    id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
+    id("com.google.devtools.ksp") version "2.3.4"
 }
 
 buildscript {
