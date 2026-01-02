868406b2f390d47a8d888fbe227dd6fa9578c8dd
thewyolar <thewyolar@gmail.com>
2026-01-03 01:20:04 +0300

    refactor: add serializable to page data class

--- app/src/main/java/javavlsu/kb/esap/esapmobile/core/domain/model/Page.kt
+++ app/src/main/java/javavlsu/kb/esap/esapmobile/core/domain/model/Page.kt
@@ -1,5 +1,8 @@
 package javavlsu.kb.esap.esapmobile.core.domain.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Page<T>(
     val content: List<T>,
     val totalPages: Int,
