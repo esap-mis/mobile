d099ad3f247e48f1b14325abd70f9580e35d6248
thewyolar <thewyolar@gmail.com>
2026-01-02 18:58:38 +0300

    chore: update project libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,12 +30,12 @@ composeCompiler {
 
 android {
     namespace = "javavlsu.kb.esap.esapmobile"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "javavlsu.kb.esap.esapmobile"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 36
         versionCode = gitCommitCount()
         versionName = "$majorVersion.$minorVersion.$patchVersion"
 
@@ -99,49 +99,49 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.1")
-    implementation("androidx.activity:activity-compose:1.9.0")
-    implementation(platform("androidx.compose:compose-bom:2024.05.00"))
+    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
+    implementation("androidx.activity:activity-compose:1.12.2")
+    implementation(platform("androidx.compose:compose-bom:2025.12.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3:1.2.1")
+    implementation("androidx.compose.material3:material3:1.4.0")
     implementation("androidx.compose.material:material-icons-extended")
 
     // Navigation Compose
-    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.navigation:navigation-compose:2.9.6")
 
     // DataStore
-    implementation("androidx.datastore:datastore-preferences:1.1.1")
+    implementation("androidx.datastore:datastore-preferences:1.2.0")
 
     // LiveData
-    implementation("androidx.compose.runtime:runtime-livedata:1.6.7")
+    implementation("androidx.compose.runtime:runtime-livedata:1.10.0")
 
     // Paging
-    val pagingVersion = "3.3.0"
+    val pagingVersion = "3.3.6"
     implementation("androidx.paging:paging-runtime-ktx:$pagingVersion")
-    implementation("androidx.paging:paging-compose:3.3.0")
+    implementation("androidx.paging:paging-compose:3.3.6")
 
     // Firebase
-    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
-    implementation("com.google.firebase:firebase-messaging:24.0.0")
+    implementation(platform("com.google.firebase:firebase-bom:34.7.0"))
+    implementation("com.google.firebase:firebase-messaging:25.0.1")
     implementation("com.google.firebase:firebase-analytics")
-    implementation("com.google.accompanist:accompanist-permissions:0.34.0")
+    implementation("com.google.accompanist:accompanist-permissions:0.37.3")
 
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.05.00"))
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2025.12.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     // Koin
-    val kotlinVersion = "3.5.3"
-    implementation("io.insert-koin:koin-core:$kotlinVersion")
-    implementation("io.insert-koin:koin-compose:1.1.1")
-    implementation("io.insert-koin:koin-androidx-compose:$kotlinVersion")
+    val koinVersion = "4.1.1"
+    implementation("io.insert-koin:koin-core:$koinVersion")
+    implementation("io.insert-koin:koin-compose:4.1.1")
+    implementation("io.insert-koin:koin-androidx-compose:$koinVersion")
 
     // Ktor
     val ktorVersion = "3.3.3"
@@ -153,7 +153,7 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
 
     // Kotlin Serialization
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
 
     // Slf4j
     implementation("org.slf4j:slf4j-android:1.7.36")
