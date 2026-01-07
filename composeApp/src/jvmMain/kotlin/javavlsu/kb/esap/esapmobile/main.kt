e20c565ec975d2960945c99227bcd2c043315bd5
thewyolar <thewyolar@gmail.com>
2026-01-08 00:17:13 +0300

    fix(desktop): enable HiDPI rendering for Compose Desktop

--- composeApp/src/jvmMain/kotlin/javavlsu/kb/esap/esapmobile/main.kt
+++ composeApp/src/jvmMain/kotlin/javavlsu/kb/esap/esapmobile/main.kt
@@ -12,19 +12,26 @@ import javavlsu.kb.esap.esapmobile.core.di.appModules
 import org.jetbrains.compose.resources.stringResource
 import org.koin.core.context.GlobalContext.startKoin
 
-fun main() = application {
-    startKoin {
-        modules(appModules)
-    }
+fun main()  {
+    System.setProperty("skiko.renderApi", "OPENGL")
+    System.setProperty("compose.desktop.rendering.multisample", "true")
+    System.setProperty("compose.desktop.rendering.vsync", "true")
+    System.setProperty("sun.java2d.uiScale.enabled", "true")
 
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = stringResource(Res.string.app_name),
-        state = WindowState(
-            position = WindowPosition(250.dp, 10.dp),
-            size = DpSize(500.dp, 700.dp)),
-        resizable = false,
-    ) {
-        App()
+    application {
+        startKoin {
+            modules(appModules)
+        }
+
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = stringResource(Res.string.app_name),
+            state = WindowState(
+                position = WindowPosition(250.dp, 10.dp),
+                size = DpSize(500.dp, 700.dp)),
+            resizable = false,
+        ) {
+            App()
+        }
     }
 }
